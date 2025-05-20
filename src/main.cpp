eccea38a25f79f5e1b1ffcb70adecb9e7495c9b0
maoqianfei <maoqianfei@maoqianfeideMacBook-Pro.local>
2025-05-20 16:56:15 +0800

    change timer logic in test

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include "graph/CompleteGraph.h"
 #include "simulation/Evaluator.h"
 #include <iostream>
-#include <chrono>
 
 void test_k_div_1() {
+    auto start = std::chrono::high_resolution_clock::now();
+
     constexpr int max_n = 20;
     constexpr int max_k = 5;
     constexpr int trials = 100;
@@ -14,24 +15,20 @@ void test_k_div_1() {
 
     for(int k = 3; k <= max_k; k++) {
         for(int n = k; n <= max_n; n += k) {
-            auto start = std::chrono::high_resolution_clock::now();
-            
             for(int t = 0; t < trials; ++t) {
                 evaluator.evaluate(n, k);
             }
-            
-            auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double, std::milli> duration = end - start;
-           
-            // print evalute time (ms)
-            std::cout << "n=" << n << ", k=" << k 
-                      << ", trials=" << trials 
-                      << ", total cost: " << duration.count() << " ms" 
-                      << std::endl;
         }
     }
 
     evaluator.printResults();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+   
+    // print evalute time (ms)
+    std::cout << "total cost: " << duration.count() << " ms" 
+              << std::endl;
 }
 
 int main() {
